ea6c06e0b2f5305dca6a433d7ad1a90f6dd1adf6
Kodori David Robert <kdr92kdr@gmail.com>
2020-04-23 21:56:02 +0300

    Updating projects view to have unique content

--- server/controllers/projectsController.js
+++ server/controllers/projectsController.js
@@ -3,46 +3,49 @@ const projects = require('../models/projectsDB')
 const projectsController = {}
 
 projectsController.index = (req, res) => {
-//   const projects = [{
-//     title: 'Dashboard',
-//     tags: ['html','css','javascript','php'],
-//     time: '12/3/2017',
-//     img: 'https://cdn.dribbble.com/users/427857/screenshots/10992168/media/4eb2102df0b27768d37e7fdfac21a240.jpg'
-//   },
-//   {
-//     title: 'BCR',
-//     tags: ['html','css','javascript','node.js'],
-//     time: '12/3/2017',
-//     img: 'https://cdn.dribbble.com/users/686119/screenshots/10987855/media/cf391fff03a675705d28ba72cde3ba25.png'
-//   },
-//   {
-//     title: 'Formula 1',
-//     tags: ['html','css','javascript','pug'],
-//     time: '12/3/2017',
-//     img: 'https://cdn.dribbble.com/users/476559/screenshots/10995737/media/47dc1297af084bb3890995a19597a333.png'
-//   },
-//   {
-//     title: 'English PL',
-//     tags: ['html','css'],
-//     time: '12/3/2017',
-//     img: 'https://cdn.dribbble.com/users/1875714/screenshots/7699472/media/36d5b73c41d8a432722c803d6296fc7a.png'
-//   },
-//   {
-//     title: 'LIDL',
-//     tags: ['html','css','javascript'],
-//     time: '12/3/2017',
-//     img: 'https://cdn.dribbble.com/users/702789/screenshots/10978529/media/4a526f9b57fb09af5f70a9c6479ca794.png'
-//   },
-//   {
-//     title: 'Mobile First',
-//     tags: ['html','css','javascript','iOS'],
-//     time: '12/3/2017',
-//     img: 'https://cdn.dribbble.com/users/1544818/screenshots/10953094/media/569dedf18ebda0d47847962be809e6d4.png'
-//   },
-// ]
+  //   const projects = [{
+  //     title: 'Dashboard',
+  //     tags: ['html','css','javascript','php'],
+  //     time: '12/3/2017',
+  //     img: 'https://cdn.dribbble.com/users/427857/screenshots/10992168/media/4eb2102df0b27768d37e7fdfac21a240.jpg'
+  //   },
+  //   {
+  //     title: 'BCR',
+  //     tags: ['html','css','javascript','node.js'],
+  //     time: '12/3/2017',
+  //     img: 'https://cdn.dribbble.com/users/686119/screenshots/10987855/media/cf391fff03a675705d28ba72cde3ba25.png'
+  //   },
+  //   {
+  //     title: 'Formula 1',
+  //     tags: ['html','css','javascript','pug'],
+  //     time: '12/3/2017',
+  //     img: 'https://cdn.dribbble.com/users/476559/screenshots/10995737/media/47dc1297af084bb3890995a19597a333.png'
+  //   },
+  //   {
+  //     title: 'English PL',
+  //     tags: ['html','css'],
+  //     time: '12/3/2017',
+  //     img: 'https://cdn.dribbble.com/users/1875714/screenshots/7699472/media/36d5b73c41d8a432722c803d6296fc7a.png'
+  //   },
+  //   {
+  //     title: 'LIDL',
+  //     tags: ['html','css','javascript'],
+  //     time: '12/3/2017',
+  //     img: 'https://cdn.dribbble.com/users/702789/screenshots/10978529/media/4a526f9b57fb09af5f70a9c6479ca794.png'
+  //   },
+  //   {
+  //     title: 'Mobile First',
+  //     tags: ['html','css','javascript','iOS'],
+  //     time: '12/3/2017',
+  //     img: 'https://cdn.dribbble.com/users/1544818/screenshots/10953094/media/569dedf18ebda0d47847962be809e6d4.png'
+  //   },
+  // ]
+  var testingVar = process.env.DB_USERNAME
 
-  res.render('projects/index',
-    {projects: projects})
+  res.render('projects/index', {
+    projects: projects,
+    testingVar: testingVar
+  })
 }
 
 projectsController.create = (req, res) => {
@@ -56,7 +59,29 @@ projectsController.store = (req, res) => {
 }
 
 projectsController.show = (req, res) => {
-  res.render('projects/show')
+  var currentTitle = req.params.title;
+  var pagetitle="default";
+
+  function searchObject(a){
+    var i;
+    for (i = 0;i<projects.length; i++) {
+      if (a == projects[i].slug) {
+         pagetitle=projects[i].title
+         console.log(pagetitle)
+         return pagetitle
+
+      }
+    }
+  }
+  pagetitle = searchObject(currentTitle);
+
+
+  res.render('projects/show', {
+    projects: projects,
+    currentTitle: currentTitle,
+    pagetitle: pagetitle
+
+  })
 }
 
 projectsController.edit = (req, res) => {
--- server/models/projectsDB.js
+++ server/models/projectsDB.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const projects = [{
+  slug: 'dashboard',
   title: 'Dashboard',
   tags: ['html','css','javascript','php'],
   time: '12/3/2017',
@@ -8,30 +9,35 @@ const projects = [{
 },
 {
   title: 'BCR',
+  slug: 'bcr',
   tags: ['html','css','javascript','node.js'],
   time: '12/3/2017',
   img: 'https://cdn.dribbble.com/users/686119/screenshots/10987855/media/cf391fff03a675705d28ba72cde3ba25.png'
 },
 {
   title: 'Formula 1',
+  slug: 'formula-1',
   tags: ['html','css','javascript','pug'],
   time: '12/3/2017',
   img: 'https://cdn.dribbble.com/users/476559/screenshots/10995737/media/47dc1297af084bb3890995a19597a333.png'
 },
 {
-  title: 'English PL',
-  tags: ['html','css'],
+  title: 'Mufi',
+  slug: 'mufi',
+  tags: ['mufi','mafi'],
   time: '12/3/2017',
   img: 'https://cdn.dribbble.com/users/1875714/screenshots/7699472/media/36d5b73c41d8a432722c803d6296fc7a.png'
 },
 {
   title: 'LIDL',
+  slug: 'lidl',
   tags: ['html','css','javascript'],
   time: '12/3/2017',
   img: 'https://cdn.dribbble.com/users/702789/screenshots/10978529/media/4a526f9b57fb09af5f70a9c6479ca794.png'
 },
 {
   title: 'Mobile First',
+  slug: 'mobile-first',
   tags: ['html','css','javascript','iOS'],
   time: '12/3/2017',
   img: 'https://cdn.dribbble.com/users/1544818/screenshots/10953094/media/569dedf18ebda0d47847962be809e6d4.png'
